d5dc4a21d7a5d71af4bd7d73b8c6bdf78af6ea83
Ilyass Ouhsseine <iouhssei@c4r6p5.1337.ma>
2024-02-20 11:30:53 +0100

    Add

--- parsing.c
+++ parsing.c
@@ -5,42 +5,22 @@ void error_quit(char *str)
 	ft_putendl_fd(str, 2);
 	return;
 }
-void check_inputs(int argc, char **str)
+void check_inputs(int argc, char **argv) 
 {
-	//i itterate the whole string finding other tahn spaces and numbers
-	//if the itteration gone to the end means that th eitterator == size of the string and its point at '\0;
-	//then return 0;
-	//else return 1;
-	int i;
+    int i; 
 	int j;
 
-	i = 1;
-	j = 0;
-	while (i < argc)
-	{
-		while(ft_isdigit(str[i][j]) || str[i][j] == ' ' || str[i][j] == '+' || str[i][j] == '-')
-			j++;
-		if(str[i][j] != '\0')
-			error_quit("Invalid set of arguments: <usage>");
-		i++;
-	}
-	// then we check the duplicate...i mean where i even start maybe a search algorithm maybeee...
-	// its bubble sort but not for storting its for search
-	// i = 0;
-	// while (i < size)
-    // {
-    //     j = 0;
-    //     while(j < size - i - 1)
-    //     {
-    //         if(str[j] == str[j + 1])
-    //             error_quit("Error : Duplicate");
-    //         j++;
-    //     }
-    //     i++;
-    // }
+    for (i = 1; i < argc; i++) {
+        j = 0;
+        while (argv[i][j]) {
+            if (!ft_isdigit(argv[i][j]) && argv[i][j] != ' ' && argv[i][j] != '+' && argv[i][j] != '-')
+                error_quit("Invalid set of arguments: <usage>");
+            j++;
+        }
+    }
 }
-//./push_swap " 1 2" "1 0" 1 3 77 9 "" = 
-char **split_args(int argc,char **argv)
+
+char **split_args(int argc, char **argv)
 {
 	int i;
 	int j;
@@ -48,68 +28,66 @@ char **split_args(int argc,char **argv)
 	char **splitted;
 	i = 2;
 
-	while(argv[i] != '\0')
+	while (argv[i] != NULL)
 	{
 		arg = ft_strjoin(argv[1], argv[i]);
 		i++;
 	}
-	free(argv);
 	splitted = ft_split(arg, ' ');
+	return (splitted);
 }
 
-int sizeof_argv(char **argv)
-{
-	int size;
-	//calculate the size of the argv splited (how much integer need to be parsed)
-
-
-	return (size);
-}
 int *atoi_ad(int argc, char **splitted)
 {
 	int i;
-//	int size;
 	int *array;
 
 	i = 0;
 	array = (int *)malloc(sizeof(*splitted) * sizeof(int));
-	if(!array)
-		return(1);
 	while (splitted[i])
 	{
 		array[i] = ft_atoi(splitted[i]);
 		i++;
 	}
-	return(array);
+	return (array);
 }
-void check_duplicate(int *array)
+
+int check_duplicate(int *array)
 {
 	int i;
 	int size;
 	int j;
 
+	size = sizeof(*array) / sizeof(int);
 	i = 0;
 	while (i < size)
-    {
-        j = 0;
-        while(j < size - i - 1)
-        {
-            if(array[j] == array[j + 1])
-                error_quit("Error : Duplicate");
-            j++;
-        }
-        i++;
-    }
+	{
+		j = 0;
+		while (j < size - i - 1)
+		{
+			if (array[j] == array[j + 1])
+				error_quit("Error : Duplicate");
+			j++;
+		}
+		i++;
+	}
+	return (size);
 }
 
 int main(int argc, char **argv)
 {
 	char **splitted;
 	int *array;
-	
-	check_inputs(argc ,argv);
+	int i;
+
+	check_inputs(argc, argv);
 	splitted = split_args(argc, argv);
+	for (i = 0; splitted[i]; i++)
+		ft_printf("%s\n", splitted[i]);
 	array = atoi_ad(argc, splitted);
-	check_duplicate(array);
+
+	int size = check_duplicate(array);
+	for (i = 0; i < size; i++)
+		ft_printf("%d\n", array[i]);
 	return (0);
 }
--- test.c
+++ test.c
@@ -3,7 +3,10 @@
 
 int main()
 {
-    char *arr[] = {"hello", "ydik", "feh"};
-    int size = sizeof(arr) / sizeof(arr[0]);
-    printf("%d\n", size);
+
+    // char *arr[] = {"hello", "ydik", "feh"};
+    // int size = sizeof(arr) / sizeof(arr[0]);
+    // printf("%d\n", size);
+    int array[] = {1,2,3,4,7,9,0,8};
+    printf("%ld", sizeof(array)/sizeof(int));
 }
