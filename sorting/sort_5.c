b6a8ca1ec18120bcda20c19f9ab2f186b2d1096f
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2024-03-20 21:46:09 +0000

    Add

--- sorting/sort_5.c
+++ sorting/sort_5.c
@@ -14,7 +14,7 @@ static int find_min(stack_i *a)
     return (min);
 }
 
-static int     get_index(stack_i *stack, int nbr)
+static int get_index(stack_i *stack, int nbr)
 {
     int index = 0;
     while (stack)
@@ -24,31 +24,34 @@ static int     get_index(stack_i *stack, int nbr)
         index++;
         stack = stack->next;
     }
-    return index;
+    return (index);
 }
 
 
 void sort_5(stack_i **a, stack_i **b, int size)
 {
-    int min;
-    (void)size;
+    int     min;
     stack_i *tmp;
-    int index = get_index(*a, find_min(*a));
-    printf("%d\n", index);
+    int     index;
+
+    index = get_index(*a, find_min(*a));
 
     while (stack_len(*a) > 3)
     {
         tmp = *a;
         min = find_min(*a);
-        // printf("%d\n", min);
         if (tmp->data == min)
             push(a, b, "pb\n");
         else
-            rotate(a, "ra\n");
+        {
+            if(index <= size/2)
+                rotate(a, "ra\n");
+            else
+                reverseRotate(a, "rra\n");
+        }
     }
     if (!stack_sorted(*a))
         sort_3(a);
-    // print_stack(*b);
     while (*b != NULL)
         push(b, a, "pa\n");
 }
