be6a6af04f8e850ed72ea69051b12a052d89952b
Ilyass Ouhsseine <101465486+samurai0lava@users.noreply.github.com>
2024-03-22 02:26:07 +0000

    Add

--- sorting/push_swap.c
+++ sorting/push_swap.c
@@ -4,7 +4,7 @@ void push_swap(stack_i **a, stack_i **b)
 {
     int len_a;
 
-    len_a = stack_len(a);
+    len_a = stack_len(*a);
 
     while(len_a > 3)
     {
@@ -13,14 +13,14 @@ void push_swap(stack_i **a, stack_i **b)
     sort_3(a);
 	while(b)
     {
-        init_param(a, b);
-        printf("b: %d\n", b->current_position);
+        init_param(*a, *b);
+        printf("b: %d\n", (*b)->current_position);
         printf("len_a: %d\n", len_a);
-        printf("len_b: %d\n", stack_len(b));
-        printf("%d\n", b->cost);
-        printf("%d\n", b->median_top);
-        printf("%d\n", b->cheapest);
-        print_stack(a->target);
+        printf("len_b: %d\n", stack_len(*b));
+        printf("%d\n", (*b)->cost);
+        printf("%d\n", (*b)->median_top);
+        printf("%d\n", (*b)->cheapest);
+        print_stack((*a)->target);
         // moves(a, b);
     }
 }
