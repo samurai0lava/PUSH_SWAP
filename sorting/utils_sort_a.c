913f0ecd58d902e7dccd576e045f087a6030acc3
samurai0lava <ouhsseineilyass@gmail.com>
2024-03-23 03:44:28 +0000

    Add

--- sorting/utils_sort_a.c
+++ sorting/utils_sort_a.c
@@ -1,2 +1,107 @@
 
 #include "../push_swap.h"
+
+static void	set_target_a(t_stack_node *a, t_stack_node *b)
+{
+	stack_i	*current_b;
+	stack_i	*target_node;
+	long	best_match;
+
+    target_node = NULL;
+	while (a)
+	{
+		best_match= LONG_MIN;
+		current_b = b;
+		while (current_b)
+		{
+			if (current_b->nbr < a->nbr 
+				&& current_b->nbr > best_match_index) 
+			{
+				best_match= current_b->data; 
+				target_node = current_b; 
+			}
+			current_b = current_b->next; 
+		}
+		if (best_match == LONG_MIN) 
+			a->target= find_min_node(b); 
+		else
+			a->target= target_node; 
+		a = a->next; 
+	}
+}
+
+void set_cheapest(stack_i *stack)
+{
+	stack_i	*current;
+	stack_i	*cheapest;
+    
+	if (!stack)
+        return;
+
+    current = stack;
+    cheapest = stack;
+    while (current) 
+	{
+        if (current->cost < cheapest->cost) 
+            cheapest = current;
+        current = current->next;
+    }
+    cheapest->cheapest = 1;
+    current = stack;
+    while (current) 
+	{
+        if (current != cheapest) 
+            current->cheapest = 0;
+        current = current->next;
+    }
+}
+
+void set_index(stack_i *a)
+{
+	int index_S;
+	int size;
+
+    if (!a)
+        return;
+	size = stack_len(a);
+	while(a)
+	{
+		index_S = get_index(a, a->data);
+		a->current_position = index_S;
+		if(index_S > size/2)
+			a->median_top = 0;
+		else
+			a->median_top = 1;
+		a = a->next
+	}
+}
+
+void set_price_a(stack_i *a, stack_i *b)
+{
+    int len_stack_a;
+	int	len_stack_b;
+	
+	len_stack_a = stack_len(a);
+    len_stack_b = stack_len(b);
+
+    while (a)
+    {
+        a->cost = a->current_position;
+        if (a->median_top == 1)
+            a->cost = len_stack_a - a->current_position;
+        else if (a->median_top == 0)
+            a->cost += a->target->current_position;
+        else
+            a->cost += len_stack_a - a->target->current_position;
+        a = a->next;
+    }
+}
+
+void init_param_a(stack_i *a, stack_i *b)
+{
+    set_index(a);
+    set_index(b);
+    set_target_a(a, b);
+    set_price_a(a, b);
+    set_cheapest(a);
+}
--- sorting/utils_sort_b.c
+++ sorting/utils_sort_b.c
@@ -1,105 +1,36 @@
 #include "../push_swap.h"
 
-void set_target(stack_i *a, stack_i *b)
+void set_target_b(stack_i *a, stack_i *b)
 {
-    stack_i *current;
-    stack_i *target;
+    stack_i *current_a;
+    stack_i *target_node;
     long    best_match;
 
-    target = NULL;
+    target_node = NULL;
     while (b)
     {
-        current = a;
+        current_a = a
         best_match = LONG_MAX;
-        while(current)
+        while(current_a)
         {
-            if(current->data < best_match && current->data > b->data)
+            if(current_a->data < best_match && current_a->data > b->data)
             {
-                best_match = current->data;
-                target = current;
+                best_match = current_a->data;
+                target_node = current_a;
             }
-            current = current->next;
+            current_a = current_a->next;
         }
-        if(best_match == LONG_MAX)
+        if (best_match == LONG_MAX)
             b->target = find_min_node(a);
         else
-            b->target = target;
+            b->target = target_node;
         b = b->next;
     }
 }
 
-void set_cheapest(stack_i *b)
-{
-	stack_i	*current;
-	stack_i	*cheapest;
-    
-	if (!b)
-        return;
-
-    current = b;
-    cheapest = b;
-    while (current) 
-	{
-        if (current->cost < cheapest->cost) 
-            cheapest = current;
-        current = current->next;
-    }
-    cheapest->cheapest = 1;
-    current = b;
-    while (current) 
-	{
-        if (current != cheapest) 
-            current->cheapest = 0;
-        current = current->next;
-    }
-}
-
-void set_index(stack_i *a)
-{
-	int index_S;
-	int size;
-
-    if (!a)
-        return;
-	size = stack_len(a);
-	while(a)
-	{
-		index_S = get_index(a, a->data);
-		a->current_position = index_S;
-		if(index_S > size/2)
-			a->median_top = 0;
-		else
-			a->median_top = 1;
-		a = a->next;
-	}
-}
-
-void set_price(stack_i *a, stack_i *b)
-{
-    int len_stack_a;
-	int	len_stack_b;
-	
-	len_stack_a = stack_len(a);
-    len_stack_b = stack_len(b);
-
-    while (b)
-    {
-        b->cost = b->current_position;
-        if (b->median_top == 1)
-            b->cost = len_stack_b - b->current_position;
-        else if (b->median_top == 0)
-            b->cost += b->target->current_position;
-        else
-            b->cost += len_stack_a - b->target->current_position;
-        b = b->next;
-    }
-}
-
-void init_param(stack_i *a, stack_i *b)
+void init_param_b(stack_i *a, stack_i *b)
 {
 	set_index(a);
 	set_index(b);
-	set_target(a ,b);
-	set_price(a,b);
-	set_cheapest(b);
+	set_target_b(a ,b);
 }
